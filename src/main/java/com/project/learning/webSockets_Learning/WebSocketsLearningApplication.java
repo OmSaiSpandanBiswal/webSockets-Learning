cce10afabbd525a0986d1f70bbecd059ab55309d
om <omsaispandanbiswal@zohomail.in>
2025-11-15 18:50:04 +0530

    init Project

---
+++ src/main/java/com/project/learning/webSockets_Learning/WebSocketsLearningApplication.java
@@ -0,0 +1,13 @@
+package com.project.learning.webSockets_Learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketsLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketsLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/learning/webSockets_Learning/WebSocketsLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.learning.webSockets_Learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketsLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
